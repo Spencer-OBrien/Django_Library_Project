7e0e06da7acc3fabe827b636f5f9d8f326cb67d5
Spencer-OBrien <spencerobrien@live.com>
2024-03-25 20:47:42 +0100

    basic setup, book modle, working on making the view work

---
+++ LibraryProject/library/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=100, unique=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=100, unique=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
---
+++ LibraryProject/library/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.all_books, name='all_books'),
+    
+]
